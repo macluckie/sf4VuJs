346a005d9e5f7b36f9993dd7f37258a203ae9ed2
macluckie <dimitri.macluckie@gmail.com>
2019-12-14 22:12:33 +0100

    getArticle

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Article;
-
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\Annotations\Get;
+use Symfony\Component\VarDumper\VarDumper;
 
 class ArticleController  extends AbstractController
 
@@ -15,9 +17,35 @@ class ArticleController  extends AbstractController
         $articles = $em->getRepository(Article::class)->findAll();
         return $this->json([
             'articles' => $articles,
-            
+
         ]);
+    }
 
+    /**
+     * HEAD Route annotation.
+     * @Get("/article/{id}")
+     */
+    public function getArticle($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $article = $em->getRepository(Article::class)->find($id);
+        $arrayArticle = [];
+        foreach ($article as $key => $value) {
+            $arrayArticle = 
+            [
+                'title' =>$article->getTitle(),
+                'subltitle' =>$article->getSubTitle(),
+                'auteur' => $article->getAuteur(),
+                'data' => $article->getDate(),
+                'contenue' =>$article->getContenue(),
+                'picture' => $article->getPicture()
 
-    } 
+            ];
+        }
+
+        return $this->json([
+            'article' => $article,
+
+        ]);
+    }
 }
